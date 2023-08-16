eec2b88fd6aac382d6b18326d962f0e6e9a98201
a-serdyuk@bitbucket.org <a-serdyuk@bitbucket.org>
2023-08-16 22:32:28 +0530

    get rid of wrapper object

--- upload.js
+++ upload.js
@@ -25,132 +25,132 @@
 import dom from 'imnd-dom';
 import ajax from 'imnd-ajax';
 
-let
+const
+  /**
+   * параметры
+   */
+  settings = {
     /**
-     * параметры
+     * путь обработчика загрузки
      */
-    settings = {
-        /**
-         * путь обработчика загрузки
-         */
-        "upload-url" : "upload.php",
-        /**
-         * id инпута файла
-         */
-        "file-id" : "file",
-        /**
-         * размер кусков, на которые делится файл.
-         */
-        "chunk-size" : 100000,
-        /**
-         * что делать после удачной загрузки
-         */
-        "complete-callback" : function() {
-            alert("Готово");
-        }
-    },
+    "upload-url": "upload.php",
     /**
-     * Считывание и загрузки фрагмента файла
-     *
-     * @param {array} options параметры
-     * @return {void}
+     * id инпута файла
      */
-    uploadBlob = function(options) {
-        const
-            file = options["file"],
-            startByte = options["startByte"],
-            stopByte = options["stopByte"],
-            chunksCount = options["chunksCount"] || 0,
-            fileNum = options["fileNum"] || 0,
-            reader = new FileReader()
-        ;
-        const
-            start = parseInt(startByte) || 0,
-            stop = parseInt(stopByte) || file.size - 1
-        ;
-        // если мы используем onloadend, нам нужно проверить readyState.
-        reader.onloadend = function() {
-            ajax.post(
-                settings["upload-url"],
-                {
-                    "data" : reader.result,
-                    "fileName" : file.name,
-                    "fileType" : file.type,
-                    "chunksCount" : chunksCount,
-                    "fileNum" : fileNum,
-                },
-                function(data) {
-                    if (data.complete===true) {
-                        settings["complete-callback"]();
-                    }
-                },
-                "json",
-                "multipart/form-data"
-            );
-        };
-        let blob;
-        if (file.slice) {
-            blob = file.slice(start, stop);
-        } else if (file.webkitSlice) {
-            blob = file.webkitSlice(start, stop);
-        } else if (file.mozSlice) {
-            blob = file.mozSlice(start, stop);
-        }
-        reader.readAsDataURL(blob);
-    },
+    "file-id": "file",
     /**
-     * Установка значения параметра
-     *
-     * @param {string} varName
-     * @param {array} options параметры
-     * @return {void}
+     * размер кусков, на которые делится файл.
      */
-    setValue = function(varName, options) {
-        if (options[varName]!==undefined) {
-            settings[varName] = options[varName];
-        }
+    "chunk-size": 100000,
+    /**
+     * что делать после удачной загрузки
+     */
+    "complete-callback": function () {
+      alert("Готово");
     }
+  },
+  /**
+   * Считывание и загрузки фрагмента файла
+   *
+   * @param {array} options параметры
+   * @return {void}
+   */
+  uploadBlob = function (options) {
+    const
+      file = options["file"],
+      startByte = options["startByte"],
+      stopByte = options["stopByte"],
+      chunksCount = options["chunksCount"] || 0,
+      fileNum = options["fileNum"] || 0,
+      reader = new FileReader()
+    ;
+    const
+      start = parseInt(startByte) || 0,
+      stop = parseInt(stopByte) || file.size - 1
+    ;
+    // если мы используем onloadend, нам нужно проверить readyState.
+    reader.onloadend = function () {
+      ajax.post(
+        settings["upload-url"],
+        {
+          "data": reader.result,
+          "fileName": file.name,
+          "fileType": file.type,
+          "chunksCount": chunksCount,
+          "fileNum": fileNum,
+        },
+        function (data) {
+          if (data.complete === true) {
+            settings["complete-callback"]();
+          }
+        },
+        "json",
+        "multipart/form-data"
+      );
+    };
+    let blob;
+    if (file.slice) {
+      blob = file.slice(start, stop);
+    } else if (file.webkitSlice) {
+      blob = file.webkitSlice(start, stop);
+    } else if (file.mozSlice) {
+      blob = file.mozSlice(start, stop);
+    }
+    reader.readAsDataURL(blob);
+  },
+  /**
+   * Установка значения параметра
+   *
+   * @param {string} varName
+   * @param {array} options параметры
+   * @return {void}
+   */
+  setValue = function (varName, options) {
+    if (options[varName] !== undefined) {
+      settings[varName] = options[varName];
+    }
+  }
 ;
 
-let upload = {
-    /**
-     * Загрузка файла по частям на сервер
-     *
-     * @return {void}
-     */
-    run : function() {
-        if (window.File && window.FileReader && window.FileList && window.Blob) {
-            const files = dom.findById(settings["file-id"]).files;
-            if (!files.length) {
-                alert("Выберите файл, пожалуйста.");
-                return;
-            }
-            const file = files[0],
-                  chunksCount = Math.ceil(file.size / settings["chunk-size"]);
-            for (let fileNum = 0; fileNum < chunksCount; fileNum++) {
-                uploadBlob({
-                    "file" : file,
-                    "startByte" : settings["chunk-size"] * fileNum,
-                    "stopByte" : settings["chunk-size"] * (fileNum + 1),
-                    "chunksCount" : chunksCount,
-                    "fileNum" : fileNum,
-                });
-            }
-        }
-    },
-
-    /**
-     * Установка значений параметров
-     *
-     * @param {obj} options параметры
-     * @return {void}
-     */
-    defaults : function(options) {
-        const varNames = ["file-id", "chunk-size", "upload-url", "complete-callback"];
-        for (const key in varNames) {
-            setValue(varNames[key], options);
-        }
+const
+  /**
+   * Загрузка файла по частям на сервер
+   *
+   * @return {void}
+   */
+  run = () => {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      const files = dom.findById(settings["file-id"]).files;
+      if (!files.length) {
+        alert("Выберите файл, пожалуйста.");
+        return;
+      }
+      const file = files[0],
+        chunksCount = Math.ceil(file.size / settings["chunk-size"]);
+      for (let fileNum = 0; fileNum < chunksCount; fileNum++) {
+        uploadBlob({
+          "file": file,
+          "startByte": settings["chunk-size"] * fileNum,
+          "stopByte": settings["chunk-size"] * (fileNum + 1),
+          "chunksCount": chunksCount,
+          "fileNum": fileNum,
+        });
+      }
     }
-};
+  },
 
-export default upload;
+  /**
+   * Установка значений параметров
+   *
+   * @param {obj} options параметры
+   * @return {void}
+   */
+  defaults = options => {
+    const varNames = ["file-id", "chunk-size", "upload-url", "complete-callback"];
+    for (const key in varNames) {
+      setValue(varNames[key], options);
+    }
+  }
+;
+
+export default { run, defaults };
